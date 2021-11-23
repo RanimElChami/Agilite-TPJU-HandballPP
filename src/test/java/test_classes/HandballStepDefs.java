64b350e2cdd2d3f5c85b0cd072efa50a9079a5bb
EstelleGrauFarre <93553815+EstelleGrauFarre@users.noreply.github.com>
2021-11-24 00:01:50 +0100

    Update HandballStepDefs.java

--- src/test/java/test_classes/HandballStepDefs.java
+++ src/test/java/test_classes/HandballStepDefs.java
@@ -1,28 +1,75 @@
 package test_classes;
 
-import java.util.Scanner;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import handball_classes.Equipe;
+import handball_classes.Joueur;
 
 public class HandballStepDefs {
 	private Equipe equipe;
+	private Joueur joueur;
 
-	@Given("^une envie")
-	public void uneEnvie(String equipe) throws Throwable {
+	private ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
+
+	private String nomEquipe;
+
+	private String nomJoueur;
+	private String prenomJoueur;
+	private int numeroJoueur;
+
+	@Given("^une equipe$")
+	public void uneEquipe () throws Throwable {
+
 	}
 
-	@When("^Estelle choisit un nom d'équipe$")
-	public void choixNomEquipe() throws Throwable {
-		Scanner input = new Scanner (System.in);
-		System.out.println("Entrer un nom d'équipe");
-		String nom = input.next();
+	@When("^Estelle choisit le nom (.*), le prénom (.*) et le numéro (\\d+) du joueur$")
+	public void choixNomPrenomNumero(String n, String p, int num) throws Throwable {
+		nomJoueur = n;
+		prenomJoueur = p;
+		numeroJoueur = num;
 	}
 
-	@Then("^L'équipe (.*) est créée$")
-	public void creerEquipe(String nom) throws Throwable {
-		this.equipe = new Equipe(nom);
+	@Then("^le joueur est créé$")
+	public void creerJoueur() throws Throwable {
+		joueur = new Joueur (nomJoueur, prenomJoueur, numeroJoueur);
 	}
+
+
+	@Given("^une envie$")
+	public void uneEnvie () throws Throwable {
+	}
+
+	@When("^Estelle choisit un nom d'equipe (.*)$")
+	public void choixNomEquipe(String n) throws Throwable {
+		nomEquipe = n;
+	}
+
+	@Then("^l'équipe est créée$")
+	public void creerEquipe() throws Throwable {
+		equipe = new Equipe (nomEquipe);
+	}
+
+	@Given("^une equipe et une joueur$")
+	public void uneEquipeEtUnJoueur () throws Throwable {
+	}
+
+	@When("^le joueur est ajouté à l'équipe (.*)$")
+	public void ajoutJoueurEquipe(String e) throws Throwable {
+		if(equipe.getNom() == e ){
+			listeJoueur = equipe.ajouterJoueur(joueur);
+		}
+		
+	}
+
+	@Then("^la liste des joueurs de l'équipe est affiché$")
+	public void afficheListeJoueur() throws Throwable {
+		System.out.println(listeJoueur);
+	}
+
+	@Then("^Un message d'erreur s'affiche$")
+	public void messageErreur() throws Throwable {
+		System.out.println("Le joueur appartient déjà à une équipe");
+	}
+
 }
